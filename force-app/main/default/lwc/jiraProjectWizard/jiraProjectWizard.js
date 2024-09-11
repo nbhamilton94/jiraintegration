99762c31ba9905e7a490646e1fa5b7e2f2d1dd23
Norman Hamilton <normanhamilton@Normans-MBP.localdomain>
2024-09-11 15:50:32 -0500

    prevent users from creating a project if it already exists

--- force-app/main/default/lwc/jiraProjectWizard/jiraProjectWizard.js
+++ force-app/main/default/lwc/jiraProjectWizard/jiraProjectWizard.js
@@ -1,8 +1,12 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { CloseActionScreenEvent } from 'lightning/actions';
 import checkJiraKeyAvailable from '@salesforce/apex/JiraProjectWizardController.checkJiraKeyAvailable';
 import createProject from '@salesforce/apex/JiraProjectWizardController.createProject';
+import {getRecord, getFieldValue} from 'lightning/uiRecordApi';
+import PROJECT_KEY from '@salesforce/schema/Account.JIRA_Project_Key__c';
+import PROJECT_NAME from '@salesforce/schema/Account.JIRA_Project_Name__c';
+import PROJECT_ID from '@salesforce/schema/Account.JIRA_Project_Id__c';
 
 export default class JiraProjectWizard extends LightningElement {
 
@@ -18,12 +22,35 @@ export default class JiraProjectWizard extends LightningElement {
 
     @api recordId;
     @api objectNameId;
+    account;
     key; 
     projectName;
     projectLead;
     available;
     notCheckedKey = true;
     projectCreationDisable = true;
+
+    @wire(getRecord, {
+        recordId: "$recordId",
+        fields: [PROJECT_KEY, PROJECT_NAME, PROJECT_ID]
+    })
+    wiredAccount({error, data}){
+        if(data){
+            this.account = data;
+            if(getFieldValue(data, PROJECT_ID)) {
+                this.dispatchEvent(new CloseActionScreenEvent());
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: "Error",
+                        message: "Jira Project Already Created",
+                        variant: "error"
+                    })
+                );
+            }
+        } else if (error) {
+            console.error(error);
+        }
+    }
 
     handleKeyChange(event){
         this.key = event.target.value;
